049f2b82585a179b1dce0641a89aedacaf1af7e6
tahirazizi34 <tahirazizi34@gmail.com>
2026-05-28 11:55:54 -0400

    Update scheduler.py

--- pipeline/scheduler.py
+++ pipeline/scheduler.py
@@ -11,7 +11,7 @@ import datetime
 import traceback
 
 # Run time in UTC — change this to whatever time you want
-RUN_TIME = os.getenv("RUN_TIME_UTC", "19:50")
+RUN_TIME = os.getenv("RUN_TIME_UTC", "16:00")
 
 
 def get_next_run(run_time_str: str) -> datetime.datetime:
