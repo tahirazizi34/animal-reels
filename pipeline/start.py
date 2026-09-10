d7abf7084cc711b51c19a50c67a40f971cb37536
tahirazizi34 <tahirazizi34@gmail.com>
2026-09-10 00:11:43 -0400

    Update start.py

--- pipeline/start.py
+++ pipeline/start.py
@@ -31,7 +31,7 @@ def run_scheduler():
     import datetime
     import traceback
 
-    RUN_TIME = os.getenv("RUN_TIME_UTC", "04:00")
+    RUN_TIME = os.getenv("RUN_TIME_UTC", "04:15")
     print(f"[Scheduler] Daily run time: {RUN_TIME} UTC")
 
     while True:
